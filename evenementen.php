e0433b5ca6aa4d488f9d5edee9732e50f68559cd
Keviinnsch <6002490@mborijnland.nl>
2022-06-09 13:17:59 +0200

    update evenementen

--- evenementen.php
+++ evenementen.php
@@ -1,33 +1,13 @@
 <?php
-$error = "";
-if(isset($_POST['submit'])) {
-    if(!empty($_POST['gebruikersnaam']) && !empty($_POST['wachtwoord'])) {
 
-        require('dbconnect.php');
-
-        $gebruikersnaam = trim($_POST['gebruikersnaam']);
-        $wachtwoord = trim($_POST['wachtwoord']);
-
-        $sql = "SELECT * FROM gebruikers WHERE username = '".$gebruikersnaam."'";
-
-        if($result = $conn->query($sql)) {
-            
-            $dbuser = $result->fetch_row();
-            $dbpass = $dbuser[2];
-            if(password_verify($wachtwoord, $dbpass)) {
-                session_start();
-                $_SESSION['ingelogd'] = true;
-                $_SESSION['username'] =  $gebruikersnaam;
-                $_SESSION['password'] = $_POST['wachtwoord'];
-                header("Location: ingelogddb.php");
-
-            } else {
-                $error = "Niet de juiste gegevens ingevuld";
-            }
-        }
-
+require('dbconnect.php');
+$sql = "SELECT * FROM evenementen";
+if($result = $conn->query($sql)){
+while ($row = $result-> fetch_row()){
+    echo "<section class='evenementen'>".$row[1]. "<img src='" .$row[5]."' alt=''></section>";
+} 
+} else {
+    echo "query werkt niet";
 }
-}
-
 
 ?>
