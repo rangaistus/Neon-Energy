1bd3110665632f94ec40eb793cdbdb04067a0bc0
rangaistus <6002029@mborijnland.nl>
2022-06-15 11:41:27 +0200

    JS, Searchbar toegevoegd.

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// input aangeven
+let search = document.getElementById('search');
+
+
+// functie aanmaken waarmee de search wordt laten zien
+searchButton.addEventListener('click', function() {
+
+    console.log(search.style.display)
+    if(search.style.display === "none") {
+        document.getElementById('search').style.display = 'block';
+    } else {
+        document.getElementById('search').style.display = 'none';
+    }
+
+});
+
