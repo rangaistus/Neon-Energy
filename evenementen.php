824004024cff08e11580cbd4f896cb22f33d341e
Keviinnsch <6002490@mborijnland.nl>
2022-06-15 11:47:45 +0200

    gefixed

--- evenementen.php
+++ evenementen.php
@@ -1,10 +1,10 @@
 <?php
 
-require('dbconnect.php');
+require('database/dbconnect.php');
 $sql = "SELECT * FROM evenementen";
 if($result = $conn->query($sql)){
 while ($row = $result-> fetch_row()){
-    echo "<section class='evenementen'>".$row[1]. "<img src='" .$row[5]."' alt=''></section>";
+    echo "<section class='evenementen'>".$row[1]. "<img src='images/postmalone.jpg" .$row[4]."' alt=''></section>";
 } 
 } else {
     echo "query werkt niet";
